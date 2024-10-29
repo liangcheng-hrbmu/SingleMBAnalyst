cf7b2909274db86bc51f8f72b7fedc70717c42f2
liang cheng <chl198478@126.com>
2024-10-29 14:54:25 +0800

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' example data
+#'
+#' A Seurat object constructd by \code{\link{read_singleMB}}
+#'
+#' @format A Seurat object of five filtered 10X single cell samples.
+#'
+"seurat.object"
---
+++ README.md
@@ -0,0 +1,9 @@
+# SingleMBAnalyst
+
+## Installation
+
+This can be installed using:
+
+```R
+devtools::install_github('PingWang7/SingleMBAnalyst')
+```
